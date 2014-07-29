dcf078b1199741e58673df15e77e26142afaf8b0
Gordon Watts <gordon@gordonandpaula.net>
2014-07-29 02:22:58 +0200

    Make some basic plots

--- extrapolator/main.cxx
+++ extrapolator/main.cxx
@@ -7,42 +7,170 @@
 #include "TString.h"
 #include "TFile.h"
 #include "TH1F.h"
+#include "TH2F.h"
 
 using namespace std;
+
+function<pair<double, double>(void)> generate_from_2d_hist(auto_ptr<TFile> &f, const string &hname);
+pair<double, double> calc_lxy(const pair<double, double> &pt, const pair<double, double> &ctau, const double &mass);
+function<double(pair<double,double>)> get_2d_lookup_function(auto_ptr<TFile> &f, const string &hname);
+
+TH1F *make_clone(TH1F *original, const string &search, const string &replace);
 
 int main()
 {
 	// Config parameters (shoudl be loaded from a file).
-	string dataset = "900_150";
+	string dataset = "600_150_plots_10m_50bin_700GeV";
 	double generated_ctau = 1.3; // meters (from the note appendix)
+	double vpion_mass = 150; // GeV.
+
+	double lxy_min = 0;
+	double lxy_max = 10000 / 1000.;
+
+	// Open the file so we can have it for later use.
+	auto input_fname = TString("../data/") + dataset + ".root";
+	auto input_file = auto_ptr<TFile>(TFile::Open(input_fname, "READ"));
 
 	// Functions we can use to generate various things
 	double ctau = generated_ctau;
 	function<pair<double, double> (void) > generate_lifetime_dual_exp = [ctau]() { return make_pair(gRandom->Exp(ctau), gRandom->Exp(ctau)); };
 
+	function<pair<double, double>(void)> generate_pt_from_final = generate_from_2d_hist(input_file, "Final_events/ana_vpi_pt1_pt2");
+
 	// Choose what we will use to do the generation.
 	function<pair<double, double>(void)> generate_lifetime = generate_lifetime_dual_exp;
+	function<pair<double, double>(void)> generate_pt = generate_pt_from_final;
+
+	// The weighting function to use
+	auto weight_lxy = get_2d_lookup_function(input_file, "Final_events/effi_Lxy1_Lxy2");
 
 	// Output file
 	auto fname = TString("../results_") + dataset + ".root";
-	auto hist_output = TFile::Open(fname, "RECREATE");
+	auto hist_output = auto_ptr<TFile>(TFile::Open(fname, "RECREATE"));
 
 	// Histograms to save.
 	auto h_ctau1 = new TH1F("ctau_1", "ctau of vpion 1", 5000, 0.0, 30.0);
-	auto h_ctau2 = (TH1F*)h_ctau1->Clone("ctau_2");
-	h_ctau2->SetTitle("ctau of vpion 2");
+	auto h_ctau2 = make_clone(h_ctau1, "1", "2");
+
+	auto h_pt1 = new TH1F("pt_1", "p_T of vpion 1", 600, 0.0, 600.0);
+	auto h_pt2 = make_clone(h_pt1, "1", "2");
+
+	auto h_lxy1 = new TH1F("lxy1", "lxy of vpion 1", 20*20, 0.0, 20.0);
+	auto h_lxy2 = make_clone(h_lxy1, "1", "2");
+
+	auto h_lxy1_maxmin = make_clone(h_lxy1, "lxy", "bad min bmax vpion's lxy for");
+	h_lxy1_maxmin->SetName("lxy1_minmax");
+	auto h_lxy2_maxmin = make_clone(h_lxy1_maxmin, "1", "2");
+
+	auto h_lxy1_badeff = make_clone(h_lxy1, "lxy", "bad eff lxy for");
+	h_lxy1_badeff->SetName("lxy1_badeff");
+	auto h_lxy2_badeff = make_clone(h_lxy1_badeff, "1", "2");
+
+	auto h_lxy1_weight = make_clone(h_lxy1, "lxy", "weighted lxy");
+	h_lxy1_weight->SetName("lxy1_weighted");
+	auto h_lxy2_weight = make_clone(h_lxy1_badeff, "1", "2");
+
+	auto h_event_weight = new TH1F("event_weight", "Event weights", 100, 0.0, 1.0);
+
+	auto h_lxy_weight_map = new TH2F("event_weight_map", "Map of efficiencies", 20 * 10, 0.0, 10.0, 20 * 10, 0.0, 10.0);
+	auto h_lxy_map = new TH2F("event_map", "Map of lxy pairs", 20 * 10, 0.0, 10.0, 20 * 10, 0.0, 10.0);
 
 	// Run the toy
-	const int toy_runs = 1000;
+	const int toy_runs = 100000;
 	for (int i_toy = 0; i_toy < toy_runs; i_toy++) {
 		// We need the pT and the lifetimes of the objects.
 		auto lt = generate_lifetime();
 		h_ctau1->Fill(lt.first);
 		h_ctau2->Fill(lt.second);
+
+		auto pt = generate_pt();		
+		h_pt1->Fill(pt.first);
+		h_pt2->Fill(pt.second);
+
+		// Straight conversion into the actual decay length.
+		auto lxy = calc_lxy(pt, lt, vpion_mass);
+		h_lxy1->Fill(lxy.first);
+		h_lxy2->Fill(lxy.second);
+
+		// Calc the weighting for this event. We have some x-checks to make sure everything is "in bounds" here.
+		if (lxy.first < lxy_min || lxy.first > lxy_max
+			|| lxy.second < lxy_min || lxy.second > lxy_max) {
+			h_lxy1_maxmin->Fill(lxy.first);
+			h_lxy2_maxmin->Fill(lxy.second);
+		}
+		else {
+			double event_eff = weight_lxy(lxy);
+			if (event_eff < 0) {
+				h_lxy1_badeff->Fill(lxy.first);
+				h_lxy2_badeff->Fill(lxy.second);
+			}
+			else {
+				// Everything is good! Fill some "interesting" plots.
+				h_event_weight->Fill(event_eff);
+				h_lxy1_weight->Fill(lxy.first, event_eff);
+				h_lxy1_weight->Fill(lxy.second, event_eff);
+				h_lxy_weight_map->Fill(lxy.first, lxy.second, event_eff);
+				h_lxy_map->Fill(lxy.first, lxy.second);
+			}
+		}
+
 	}
 
 	// Make sure everything is correctly saved.
 	hist_output->Write();
 	hist_output->Close();
 	return 0;
+}
+
+// Helper function for making duplicate identical histograms.
+TH1F *make_clone(TH1F *original, const string &search, const string &replace)
+{
+	TString newName(original->GetName());
+	TString newTitle(original->GetTitle());
+
+	newName.ReplaceAll(search.c_str(), replace.c_str());
+	newTitle.ReplaceAll(search.c_str(), replace.c_str());
+
+	TH1F *result = (TH1F*)original->Clone(newName);
+	result->SetTitle(newTitle);
+	return result;
+}
+
+// Get a 2D histo from a file and use it to generate a random 2D distribution.
+function<pair<double, double>(void)> generate_from_2d_hist(auto_ptr<TFile> &f, const string &hname)
+{
+	auto h = (TH2F*) f->Get(hname.c_str());
+	if (h == nullptr) {
+		throw runtime_error(string("Unable to load histogram") + hname);
+	}
+	return[h]() {double p1, p2; h->GetRandom2(p1, p2); return make_pair(p1, p2); };
+}
+
+// Given pt and ctau, find out where the thing decayed.
+pair<double, double> calc_lxy(const pair<double, double> &pt, const pair<double, double> &ctau, const double &mass)
+{
+	return make_pair(pt.first / mass * ctau.first, pt.second / mass * ctau.second);
+}
+
+// Get a bin number for the bin we need to look at. Return a -1 if we over flow.
+function<double(pair<double, double>)> get_2d_lookup_function(auto_ptr<TFile> &f, const string &hname)
+{
+	auto h = (TH2F*)f->Get(hname.c_str());
+	if (h == nullptr) {
+		throw runtime_error(string("Unable to load histogram") + hname);
+	}
+	return [h](pair<double, double> l) {
+		int i_bin_1 = h->GetXaxis()->FindBin(l.first);
+		int i_bin_2 = h->GetXaxis()->FindBin(l.second);
+
+		// If overflow, bail!
+		if (i_bin_1 > h->GetXaxis()->GetNbins())
+			return -1.0;
+		if (i_bin_2 > h->GetYaxis()->GetNbins())
+			return -1.0;
+		if (i_bin_1 < 0 || i_bin_2 < 0)
+			return -1.0;
+
+		return h->GetBinContent(i_bin_1, i_bin_2);
+	};
 }
